a003aafba0cd9ec157d2eeacbf50d868aaac9752
DaniDev77 <daneelvilela@gmail.com>
2026-08-13 11:44:57 -0300

    Classe usuario com Identity

--- Sistema/Models/Usuario.cs
+++ Sistema/Models/Usuario.cs
@@ -1,4 +1,6 @@
-﻿namespace Sistema.Models
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Sistema.Models
 {
     public class Usuario
     {
@@ -6,9 +8,12 @@
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
-        public Guid FuncaoId { get; set; }
+        public string FuncaoNome { get; set; }
         public Funcao? Funcao { get; set; }
         public string? Password { get; set; } = string.Empty;
-
+        
+        // Relacionamento com o IdentityUser
+        public Guid? AppUserId { get; set; }
+        public IdentityUser? IdentityUser { get; set; }
     }
 }
