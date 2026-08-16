6bc2f9e4889ba85924a665f75bb1c0781b57a1be
DaniDev77 <daneelvilela@gmail.com>
2026-08-16 11:02:16 -0300

    Classe para Categorizar os produtos no sistema

---
+++ Sistema/Models/TipoProduto.cs
@@ -0,0 +1,10 @@
+﻿namespace Sistema.Models
+{
+    public class TipoProduto
+    {
+        public int TipoProdutoId { get; set; }
+        public string TipoNome { get; set; } = string.Empty;
+        public string TipoDescricao { get; set; } = string.Empty;
+
+    }
+}
