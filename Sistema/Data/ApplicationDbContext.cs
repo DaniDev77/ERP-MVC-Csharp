f3e2722ab5cf0736659148d925d63e454586339b
DaniDev77 <daneelvilela@gmail.com>
2026-08-20 13:49:30 -0300

    migration de Usuarios e Funções

--- Sistema/Data/ApplicationDbContext.cs
+++ Sistema/Data/ApplicationDbContext.cs
@@ -10,15 +10,15 @@ namespace Sistema.Data
         public DbSet<Funcao> Funcoes { get; set; }
         public DbSet<Fonecedor> Fonecedores { get; set; }
         public DbSet<TipoProduto> TiposProdutos { get; set; }
-       // public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
             builder.Entity<Funcao>().ToTable("Funcao");
             builder.Entity<Fonecedor>().ToTable("Fonecedor");
-            builder.Entity<TipoProduto>().ToTable("TipoProduto");
-          //  builder.Entity<Usuario>().ToTable("Usuario");
+            builder.Entity<TipoProduto>().ToTable("TipoProduto");    
+            builder.Entity<Usuario>().ToTable("Usuario");
             // Configurações adicionais do modelo podem ser feitas aqui
         }
     }
